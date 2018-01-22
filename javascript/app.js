41ef5e8feeff08d45aca9975ab5db07faa6f21ce
Kimberly Keller <keller.kimberly@gmail.com>
2018-01-21 21:28:20 -0700

    cleaned up footer and js file

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 20,
-    backSpeed: 20,
-    startDelay: 1000,
-    loop: false,
-    loopCount: Infinity,
-  });
-
-});
-
 function prettyLog(str) {
   console.log('%c ' + str, 'color: green; font-weight: bold;');
 }
